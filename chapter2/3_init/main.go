531e586ef019d0aa47bc43e6b708981d9974cdce
yashirook <yashiro.kentaro@gmail.com>
2024-04-03 22:58:46 +0900

    P19
    エラーが発生し得ないような処理はOK
    goのblogもそうなっているみたい

--- chapter2/3_init/main.go
+++ chapter2/3_init/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	_ "init/pkg/foo"
 	"init/pkg/redis"
+	"net/http"
 )
 
 var a = func() int {
@@ -17,6 +18,26 @@ func init() {
 
 func init() {
 	fmt.Println("init second")
+}
+
+func init() {
+	redirect := func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+	http.HandleFunc("/blog", redirect)
+	http.HandleFunc("/blog/", redirect)
+
+	static := http.FileServer(http.Dir("static"))
+	http.Handle("/favicon.ico", static)
+	http.Handle("/fonts.css", static)
+	http.Handle("/fonts/", static)
+
+	http.Handle("/lib/godoc", http.StripPrefix("/lib/godoc", http.HandlerFunc(staticHander)))
+}
+
+func staticHander(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
 }
 
 func main() {
