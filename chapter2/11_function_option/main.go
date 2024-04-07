eb64ed2b69ed54caad876f03b6d75ae5a7efcb66
yashirook <yashiro.kentaro@gmail.com>
2024-04-07 22:42:19 +0900

    P48
    Builder Partternで関数を呼び出した

--- chapter2/11_function_option/main.go
+++ chapter2/11_function_option/main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"option/httplib"
 )
 
 func main() {
 	fmt.Println("function option pattern")
+	builder := httplib.ConfigBuilder{}
+	builder.Port(8080)
+	cfg, err := builder.Build()
+	if err != nil {
+		log.Fatalf("Failed to build config: %v", err)
+	}
+
+	server := NewServer("localhost", cfg)
+	log.Printf("server: %v", server)
 }
 
-type Config struct {
+type Server struct {
 	Port int
 }
 
-type Server struct{}
-
 // Configパターン
-func NewServer(addr string, cfg Config) Server {
-	return Server{}
+func NewServer(addr string, cfg httplib.Config) Server {
+	return Server{
+		Port: cfg.Port,
+	}
 }
