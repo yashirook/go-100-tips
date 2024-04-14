212b367b5cf4f76574a4da69868d0ff4d79a4764
yashirook <yashiro.kentaro@gmail.com>
2024-04-14 23:28:57 +0900

    P66
    オーバーフローする際にpanicを起こすヘルパーを用意しておく

--- chapter3/2_number_overflow/main.go
+++ chapter3/2_number_overflow/main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	var counter int32 = math.MaxInt32
 	fmt.Printf("counter: %d\n", counter)
-	counter++
-	fmt.Printf("incremented counter: %d\n", counter)
+	fmt.Printf("incremented counter: %d\n", Inc32(counter))
+}
+
+func Inc32(counter int32) int32 {
+	if counter == math.MaxInt32 {
+		panic("int32 overflow!")
+	}
+	return counter + 1
 }
